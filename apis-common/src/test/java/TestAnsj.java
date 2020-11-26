51013357aae2fd32816954273966ce5a2390f759
yangj0020 <43334889+yangj0020@users.noreply.github.com>
2020-11-26 20:00:35 +0800

    测试

--- apis-common/src/test/java/TestAnsj.java
+++ apis-common/src/test/java/TestAnsj.java
@@ -1,7 +1,6 @@
 import org.ansj.domain.Result;
 import org.ansj.domain.Term;
 import org.ansj.splitWord.analysis.ToAnalysis;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -14,7 +13,7 @@ public class TestAnsj {
     public static void main(String[] args) {
 
 //        String str = "明天下午3点到5点去计算机学院进行计算机组成原理考试" ;
-        String str = "下午3点去教五上早八";
+        String str = "下午3点在樱顶上课";
         Result parse = ToAnalysis.parse(str);
         System.out.println(ToAnalysis.parse(str));
         List<Term> terms = parse.getTerms();
